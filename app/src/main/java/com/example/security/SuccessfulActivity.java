38ca93b91a250b45ae088f455595f5f019c110d2
Huy Pham <huypt110402@gmail.com>
2024-06-01 14:42:02 +0700

    backtoMan

--- app/src/main/java/com/example/security/SuccessfulActivity.java
+++ app/src/main/java/com/example/security/SuccessfulActivity.java
@@ -1,6 +1,9 @@
 package com.example.security;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SuccessfulActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_successful);
+        button = findViewById(R.id.backToMain);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuccessfulActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
